5256d4a6dc4e2d7b84d0e552b06c39cf562ffd25
Prasad-Indika <prasad.hotmail@gmail.com>
2023-09-16 12:28:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+function btnSavingAccount(){
+    const val = `
+    <div class="cont1">
+     
+    </div>
+    <div class="cont2">
+      <img src="Assets/btnPic.PNG" alt="">
+    </div>`
+
+    document.getElementById("descriptionPanel").innerHTML = val
+}
+
+function btnDeposit(){
+    const val = ` 
+    <div class="cont1">
+     
+    </div>
+    <div class="cont2">
+      <img src="Assets/deposit.PNG" alt="">
+    </div>`
+
+    document.getElementById("descriptionPanel").innerHTML = val
+}
+
+
+function btnCurrentAccount(){
+    const val = ` 
+    <div class="cont1">
+     
+    </div>
+    <div class="cont2">
+      <img src="Assets/current.PNG" alt="">
+    </div>`
+
+    document.getElementById("descriptionPanel").innerHTML = val
+}
+
+
+function btnCards(){
+    const val = `
+    <div class="cont1">
+     
+    </div>
+    <div class="cont2">
+      <img src="Assets/cards.PNG" alt="">
+    </div>`
+
+    document.getElementById("descriptionPanel").innerHTML = val
+}
+
+
+function btnLoans(){
+    const val = `
+    <div class="cont1">
+     
+    </div>
+    <div class="cont2">
+      <img src="Assets/loans.PNG" alt="">
+    </div>`
+
+    document.getElementById("descriptionPanel").innerHTML = val
+}
---
+++ menu.js
@@ -0,0 +1,93 @@
+
+function savingAcount(){
+
+    document.getElementById("list1").innerHTML = `
+      <p>Main Accounts</p>
+      <a class="list-group-item" href="#">Regular Saving</a>
+      <a class="list-group-item" href="#">Super Save Acc</a>
+      <a class="list-group-item" href="#">Power Bonus</a>
+      <a class="list-group-item" href="#">Personal Foreign</a>
+      <a class="list-group-item" href="#">Achiever Salary Acc</a>
+   `
+
+   document.getElementById("list2").innerHTML = `
+      <p>Senior Citizens Acc</p>
+      <a class="list-group-item" href="#">Yasasa Pensioner Acc</a>
+      <a class="list-group-item" href="#">Udara Senior Acc</a>
+      
+   `
+
+   document.getElementById("list3").innerHTML = `
+    
+      <p>Minor and Youth</p>
+      <a class="list-group-item" href="#">Vibe Saving Acc</a>
+      <a class="list-group-item" href="#">Dotcom Teen Acc</a>
+      <a class="list-group-item" href="#">Arunalu Children</a>
+      <a class="list-group-item" href="#">Forein Currency</a>
+      <a class="list-group-item" href="#">Isuru Minors</a>
+   `
+
+   document.getElementById("list4").innerHTML = `
+      <p>Woments Saving </p>
+      <a class="list-group-item" href="#"> Anagi saving Acc</a>
+      
+   `
+
+}
+
+function deposits(){
+
+    document.getElementById("list1").innerHTML = `
+    <p>Invesments</p>
+    <a class="list-group-item" href="#">Millionaire Invesment plan</a>
+    <a class="list-group-item" href="#">Millionaire Invesment plan in Foreingn Currency</a>
+    <a class="list-group-item" href="#">Treasury Bills</a>
+    <a class="list-group-item" href="#">Treasury Bonds</a>
+    <a class="list-group-item" href="#">Money Market Accounts</a>
+ `
+
+ document.getElementById("list2").innerHTML = `
+    <p>Fixed Deposits</p>
+    <a class="list-group-item" href="#">Special 100 days</a>
+    <a class="list-group-item" href="#">Personal Foreign Currency</a>
+    <a class="list-group-item" href="#">Fixed Deposits</a>
+    
+ `
+
+ document.getElementById("list3").innerHTML = `
+  
+    <p>Minor and Youth</p>
+    <a class="list-group-item" href="#">Forex Plus</a>
+    <a class="list-group-item" href="#">Call Deposits</a>
+   
+ `
+
+ document.getElementById("list4").innerHTML = `
+       
+ `
+
+}
+
+
+function currentAcc(){
+   document.getElementById("list1").innerHTML = `
+   <p>Current Accounts</p>
+   <a class="list-group-item" href="#">Current Accounts</a>
+   <a class="list-group-item" href="#">Achiever Current Account</a>
+   
+`
+
+document.getElementById("list2").innerHTML = ``
+
+document.getElementById("list3").innerHTML = ``
+
+document.getElementById("list4").innerHTML = ``
+
+}
+
+
+function cards(){
+   
+}
+
+
